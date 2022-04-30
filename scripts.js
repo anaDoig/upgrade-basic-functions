bbbc08404af7e0577d290fc105feb3eca236ac19
Ana Doig <ana.doig@bootcamp-upgrade.com>
2022-04-30 17:47:00 +0200

    Iteration#1

---
+++ scripts.js
@@ -0,0 +1,9 @@
+/* Iteración 1 */
+function compare(numberOne , numberTwo) {
+    if (numberOne < numberTwo) {
+        console.log(numberTwo);
+    } else {
+        console.log(numberOne);
+    }
+}
+compare(100, 120);
